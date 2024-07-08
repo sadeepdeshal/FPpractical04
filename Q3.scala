a60ee42e0bd58e669ea2c77d264b3fe8c81bdad5
sadeepdeshal <140165441+sadeepdeshal@users.noreply.github.com>
2024-07-08 22:05:03 +0530

    Add files via upload

---
+++ Q3.scala
@@ -0,0 +1,27 @@
+package FP_Prac04
+
+import scala.io.StdIn
+
+object Q3 {
+
+  def toUpper(s: String): String = s.toUpperCase
+
+  def toLower(s: String): String = s.toLowerCase
+
+  def formatNames(name: String)(formatFunction: String => String): String = {
+    formatFunction(name)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val benny = formatNames("Benny")(toUpper)
+    val niroshan = formatNames("Niroshan")(name => name.take(2).toUpperCase + name.drop(2))
+    val saman = formatNames("Saman")(toLower)
+    val kumara = formatNames("Kumara")(name => name.take(name.length - 1) + name.last.toUpper)
+
+    println(benny)
+    println(niroshan)
+    println(saman)
+    println(kumara)
+  }
+
+}
